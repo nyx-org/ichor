dce9b669476f153e32e41a5489cef301e4434a2e
lg <abbix@riseup.net>
2023-01-06 22:20:02 -0500

    use liballoc

--- src/alloc.c
+++ src/alloc.c
@@ -1,56 +1,30 @@
 #include <ichor/alloc.h>
 #include <ichor/debug.h>
-#include <ichor/libheap.h>
+#include <ichor/liballoc.h>
 #include <ichor/syscalls.h>
 #include <ichor/vm.h>
 
-void *heap_hook_alloc_block(void *ctx, size_t size)
+void *liballoc_alloc(size_t n)
 {
-    (void)ctx;
-
-    VmObject object = sys_vm_create(size, 0, 0);
+    VmObject object = sys_vm_create(n * 4096, 0, 0);
     sys_vm_map(NULL, &object, VM_PROT_READ | VM_PROT_WRITE, -1, VM_MAP_ANONYMOUS);
 
     return object.buf;
 }
 
-void heap_hook_free_block(void *ctx, void *ptr, size_t size)
+int liballoc_free(void *ptr, size_t n)
 {
-    (void)ctx;
-    // munmap(ptr, size);
+    (void)ptr;
+    (void)n;
+    return 0;
 }
 
-void heap_hook_error(void *ctx, const char *msg)
+int liballoc_lock()
 {
-    (void)ctx;
-    ichor_debug("%s", msg);
+    return 0;
 }
 
-struct Heap _heap = {
-    .alloc = heap_hook_alloc_block,
-    .free = heap_hook_free_block,
-    .error = heap_hook_error,
-};
-
-void *ichor_malloc(size_t size)
+int liballoc_unlock()
 {
-    void *res = heap_alloc(&_heap, size);
-    return res;
-}
-
-void ichor_free(void *ptr)
-{
-    heap_free(&_heap, ptr);
-}
-
-void *ichor_realloc(void *ptr, size_t size)
-{
-    void *res = heap_realloc(&_heap, ptr, size);
-    return res;
-}
-
-void *ichor_calloc(size_t num, size_t size)
-{
-    void *res = heap_calloc(&_heap, num, size);
-    return res;
+    return 0;
 }
